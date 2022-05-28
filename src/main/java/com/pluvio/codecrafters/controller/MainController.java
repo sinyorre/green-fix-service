2bb8f7ee874b95f734b59efd73b92b7dc3857ce5
sinyorre <sinanalagoz999@gmail.com>
2022-05-28 21:36:19 +0300

    commit

--- src/main/java/com/pluvio/codecrafters/controller/MainController.java
+++ src/main/java/com/pluvio/codecrafters/controller/MainController.java
@@ -15,7 +15,10 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.nio.charset.StandardCharsets;
@@ -56,50 +59,45 @@ public class MainController {
         int page = 1;
         int maxPage = 2;
         List<QuestionEntity> questionEntityList = new ArrayList<>();
-        List<String> tags = new ArrayList<String>() {{
-            add("nodejs");
-        }};
 
-        for (String tag : tags) {
-            while (maxPage >= page) {
-                String URL = "https://api.stackexchange.com/2.3/questions?pagesize=100&page=" + page +
-                        "&fromdate=" + fromdate + "&todate=" + todate + "&order=desc&sort=votes&tagged=" + tag +
-                        "&site=stackoverflow&filter=" + FILTER;
+        while (maxPage >= page) {
+            String URL = "https://api.stackexchange.com/2.3/questions?pagesize=100&page=" + page +
+                    "&fromdate=" + fromdate + "&todate=" + todate + "&order=desc&sort=votes&tagged=" + tagged +
+                    "&site=stackoverflow&filter=" + FILTER;
 
-                restTemplate.getMessageConverters()
-                        .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
-                ResponseEntity<ResponseItem> questionsResponseEntity = restTemplate.getForEntity(URL, ResponseItem.class);
-                ResponseItem questions = questionsResponseEntity.getBody();
-                assert questions != null;
-                System.out.println("Remaining : " + questions.getQuota_remaining());
-                questions.getItems().forEach(question -> {
-                    long questionId = question.getQuestion_id();
+            restTemplate.getMessageConverters()
+                    .add(0, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+            ResponseEntity<ResponseItem> questionsResponseEntity = restTemplate.getForEntity(URL, ResponseItem.class);
+            ResponseItem questions = questionsResponseEntity.getBody();
+            assert questions != null;
+            System.out.println("Remaining : " + questions.getQuota_remaining());
+            questions.getItems().forEach(question -> {
+                long questionId = question.getQuestion_id();
 
-                    /* Question */
-                    long creation_date = question.getCreation_date();
-                    Date date = new Date(1220227200L * 1000);
+                /* Question */
+                long creation_date = question.getCreation_date();
+                Date date = new Date(1220227200L * 1000);
 
-                    // new Date(1653008124L * 1000).toInstant() JAVA
-                    // new Date(1653059311 * 1000).toUTCString() JS
-                    if (question.getAccepted_answer_id() > 0 && question.getView_count() > 1) {
-                        QuestionEntity questionEntity = modelMapper.map(question, QuestionEntity.class);
-                        List<AnswerEntity> answersQuestionEntity = questionEntity.getAnswers();
-                        List<Answer> answers = question.getAnswers();
-                        for (int i = 0; i < answers.size(); i++) {
-                            Answer answer = answers.get(i);
-                            String link = answer.getOwner().getLink();
-                            String name = answer.getOwner().getDisplay_name();
-                            answersQuestionEntity.get(i).setOwnerLink(link);
-                            answersQuestionEntity.get(i).setOwnerName(name);
-                        }
-                        questionEntityList.add(questionEntity);
+                // new Date(1653008124L * 1000).toInstant() JAVA
+                // new Date(1653059311 * 1000).toUTCString() JS
+                if (question.getAccepted_answer_id() > 0 && question.getView_count() > 1) {
+                    QuestionEntity questionEntity = modelMapper.map(question, QuestionEntity.class);
+                    List<AnswerEntity> answersQuestionEntity = questionEntity.getAnswers();
+                    List<Answer> answers = question.getAnswers();
+                    for (int i = 0; i < answers.size(); i++) {
+                        Answer answer = answers.get(i);
+                        String link = answer.getOwner().getLink();
+                        String name = answer.getOwner().getDisplay_name();
+                        answersQuestionEntity.get(i).setOwnerLink(link);
+                        answersQuestionEntity.get(i).setOwnerName(name);
                     }
-                });
+                    questionEntityList.add(questionEntity);
+                }
+            });
 
-                long a = questions.getTotal() / questions.getPage_size();
-                maxPage = (int) Math.ceil(a);
-                page++;
-            }
+            long a = questions.getTotal() / questions.getPage_size();
+            maxPage = (int) Math.ceil(a);
+            page++;
         }
         itemRepository.saveAll(questionEntityList);
         return "Hello";
